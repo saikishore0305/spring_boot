bbe5f7117020fff5b2b7cdfef5213aa0fe244851
saikishore0305 <saikishore0305@gmail.com>
2018-06-14 13:38:53 +0530

    updated the swager2.0

--- src/main/java/com/kaluzny/SwaggerConfig.java
+++ src/main/java/com/kaluzny/SwaggerConfig.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
-import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
